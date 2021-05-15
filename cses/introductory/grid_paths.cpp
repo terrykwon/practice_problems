3be7354ade36c27817aea3eff1e7b4e771c2ba64
Terry Kwon <terrykkwon@gmail.com>
2021-05-15 18:49:31 +0900

    Solve grid paths

--- cses/introductory/grid_paths.cpp
+++ cses/introductory/grid_paths.cpp
@@ -3,94 +3,103 @@
 using namespace std;
 
 const int N = 7;
-bool grid[N][N] = {};  // Initialized as 0
+bool visited[N][N] = {};  // Initialized as 0
 string directions;
 
 int paths = 0;
+long long dfs_calls = 0;
 
 
-// TODO
-bool early_terminate(int r, int c, int direction) {
-
+bool valid(int r, int c) {
+    return r >= 0 && r < N && c >= 0 && c < N && !visited[r][c];
 }
 
+// Prune
+bool early_terminate(int r, int c) {
+    // if (r == N-1 && c == 0 && pos < N*N-1) {
+    //     return true;
+    // }
 
-bool movable(int r, int c, int direction) {
-    if (direction == 'L') {
-        return c >= 1 && !grid[r][c-1];
-    } else if (direction == 'R') {
-        return c <= N-2 && !grid[r][c+1]; 
-    } else if (direction == 'U') {
-        return r >= 1 && !grid[r-1][c];
-    } else {  // direction == 'D'
-        return r <= N-2 && !grid[r+1][c];
+    // If there are empty squares to the left and right but we cannot go
+    // forward or back, then this search can be terminated.
+    if (valid(r-1, c) && valid(r+1, c) && 
+            !valid(r, c+1) && !valid(r, c-1)) {
+        return true;
+    } 
+    if (valid(r, c-1) && valid(r, c+1) && 
+            !valid(r-1, c) && !valid(r+1, c)) {
+        return true;
     }
+    return false;
 }
 
 void print_grid() {
     for (int r = 0; r < N; r++) {
         for (int c = 0; c < N; c++) {
-            cout << grid[r][c] << " ";
+            cout << visited[r][c] << " ";
         }
         cout << endl;
     }
     cout << endl;
 }
 
-
-// Marking *current* position is easier than marking next!
+/**
+ * Code efficiency here matters--TLE if unlucky.
+ * 
+ * Checking BEFORE calling the DFS function (as opposed to checking at the
+ * start of DFS) is a bit faster? No idea...
+ */
 void dfs(int r, int c, int pos) {
-    if (pos == N*N - 1) {
-        if (r == N-1 && c == 0) {  // arrived at bottom left
-            paths += 1;
-            cout << paths << endl;
-        } 
-        return;
-    }
-
-}
-
-void search(int r, int c, int pos) {
     // print_grid();
+    // if (!valid(r, c)) {
+    //     return;
+    // }
 
-    if (pos == N*N - 1) {
-        if (r == N-1 && c == 0) {  // arrived at bottom left
+    // if (early_terminate(r, c, pos)) {
+    //     return;
+    // }
+
+    if (r == N-1 && c == 0) {
+        if (pos == N*N-1) {
             paths += 1;
-            cout << paths << endl;
-        } 
+        }
         return;
     }
 
-    char direction = directions[pos];
-        
-    if (direction == 'D'|| direction == '?') {
-        if (movable(r, c, 'D')) {
-            grid[r+1][c] = true;
-            search(r+1, c, pos+1);
-            grid[r+1][c] = false;
-        }
-    } 
-    if (direction == 'U' || direction == '?') {
-        if (movable(r, c, 'U')) {
-            grid[r-1][c] = true;
-            search(r-1, c, pos+1);
-            grid[r-1][c] = false;
-        }
-    }
-    if (direction == 'L' || direction == '?') {
-        if (movable(r, c, 'L')) {
-            grid[r][c-1] = true;
-            search(r, c-1, pos+1);
-            grid[r][c-1] = false;
-        }
-    } 
-    if (direction == 'R' || direction == '?') {
-        if (movable(r, c, 'R')) {
-            grid[r][c+1] = true;
-            search(r, c+1, pos+1);
-            grid[r][c+1] = false;
+    // if (pos == N*N-1) {
+    //     if (r == N-1 && c == 0) {
+    //         paths += 1;
+    //         // cout << paths << endl;
+    //     }
+    //     return;
+    // }
+
+    // dfs_calls++;
+
+    visited[r][c] = true;
+    char d = directions[pos];
+
+    if (d == 'U' || d == '?') {
+        if (valid(r-1, c) && !early_terminate(r-1, c)) {
+            dfs(r-1, c, pos+1);
         }
     }
+    if (d == 'D' || d == '?') {
+        if (valid(r+1, c) && !early_terminate(r+1, c)) {
+            dfs(r+1, c, pos+1);
+        }
+    }
+    if (d == 'L' || d == '?') {
+        if (valid(r, c-1) && !early_terminate(r, c-1)) {
+            dfs(r, c-1, pos+1);
+        }
+    }
+    if (d == 'R' || d == '?') {
+        if (valid(r, c+1) && !early_terminate(r, c+1)) {
+            dfs(r, c+1, pos+1);
+        }
+    }
+    visited[r][c] = false;
 }
 
 
@@ -101,8 +110,8 @@ void search(int r, int c, int pos) {
 int main() {
     cin >> directions;
 
-    grid[0][0] = true;
-    search(0, 0, 0);
+    dfs(0, 0, 0);
     
     cout << paths << endl;
+    // cout << dfs_calls << endl;
 }
