172680783d4f2a7ea38dd7c6444cc7483185f5a5
Terry Kwon <terrykkwon@gmail.com>
2022-08-21 13:22:38 -0400

    Solve

---
+++ leetcode/11_container_with_most_water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+
+    cout << s.maxArea(height) << endl;
+}
