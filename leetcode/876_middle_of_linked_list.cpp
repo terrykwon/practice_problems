09ede8f94aec6008d33ac724e8bc8d2e20160ace
Terry Kwon <terrykkwon@gmail.com>
2022-08-28 18:19:00 -0400

    Solve

---
+++ leetcode/876_middle_of_linked_list.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* l = head;       
+        ListNode* r = head;       
+    }
+};
