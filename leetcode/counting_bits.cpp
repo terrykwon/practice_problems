0cacf53c37c714f7898c70a41e149ed13cd90ece
Terry Kwon <terrykkwon@gmail.com>
2022-08-29 20:46:39 -0400

    Solve

--- leetcode/counting_bits.cpp
+++ leetcode/counting_bits.cpp
@@ -5,15 +5,11 @@ using namespace std;
 class Solution {
 public:
     vector<int> countBits(int n) {
-        if (n == 0) {
-            return vector<int> {0};
-        }
         vector<int> results(n+1);
         results[0] = 0;
-        results[1] = 1;
 
-        int x = 2;
-        for (int i = 2; i <= n; i++) {
+        int x = 1;
+        for (int i = 1; i <= n; i++) {
             if (i == 2*x) {
                 x *= 2;
             }
