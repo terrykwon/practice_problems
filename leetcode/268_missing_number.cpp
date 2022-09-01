eb7aaf0b6699d8914f5cb7ca1b268e48ce7774c5
Terry Kwon <terrykkwon@gmail.com>
2022-09-01 17:43:30 -0400

    Solve

---
+++ leetcode/268_missing_number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int full_sum = n * (n+1) / 2;
+        int actual_sum = accumulate(nums.begin(), nums.end(), 0);
+        return full_sum - actual_sum;
+    }
+};
