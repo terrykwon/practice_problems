f08b28e202a6d21f1b6f1da6d2e3c89b4628deba
Terry Kwon <terrykkwon@gmail.com>
2022-09-12 13:00:24 -0400

    Solve

---
+++ leetcode/102_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result; 
+        queue<TreeNode*> q;
+
+        if (root) {
+            q.push(root);
+        }
+
+        while (!q.empty()) {
+            int num_nodes_in_level = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < num_nodes_in_level; i++) {
+                TreeNode* n = q.front();
+                q.pop();
+                level.push_back(n->val);
+
+                if (n->left) {
+                    q.push(n->left);
+                }
+                if (n->right) {
+                    q.push(n->right);
+                }
+            }
+            result.push_back(level);
+        }
+        return result;
+    }
+};
