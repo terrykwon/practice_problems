59e007ec273b6c045b94f95048b54b54a8feb797
Terry Kwon <terrykkwon@gmail.com>
2022-11-24 07:38:54 -0500

    Solve

--- leetcode/797_all_paths_from_source_to_target.cpp
+++ leetcode/797_all_paths_from_source_to_target.cpp
@@ -32,4 +32,53 @@ public:
         }
 
     }
+
+    int M[16] = {};
+    /**
+     * Variation: Just counting the number of paths.
+     * Can be done in O(V+E). 
+     */
+    int countAllPaths(vector<vector<int>>& graph) {
+        int n = graph.size();
+        fill_n(M, 16, -1);  // Also acts as a visited array
+
+        dfs2(graph, n, 0);
+
+        return M[0];
+    }
+    
+    void dfs2(vector<vector<int>>& graph, int n, int u) {
+        cout << "dfs " << u << endl;
+        if (u == n-1) {
+            M[u] = 1;
+            return;
+        }
+
+        if (M[u] == -1) {
+            M[u] = 0;
+            for (int v: graph[u]) {
+                dfs2(graph, n, v);
+                M[u] += M[v];
+            }
+        }
+    }
 };
+
+
+int main() {
+    Solution s;
+    vector<vector<int>> graph = {
+        {1,2,3},
+        {4},
+        {4},
+        {4},
+        {5,6,7},
+        {8},
+        {8},
+        {8},
+        {}
+    };
+
+    cout << s.countAllPaths(graph) << endl;
+    return 0;
+}
